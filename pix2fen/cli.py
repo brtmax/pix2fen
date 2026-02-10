362883bd064c051e011184bab7df4700875a34a3
Max Bretschneider <maxbretschneider@mailbox.org>
2026-02-10 17:22:24 +0100

    Added screenshot -> board cropping to main function

--- pix2fen/cli.py
+++ pix2fen/cli.py
@@ -2,22 +2,25 @@ import sys
 import cv2
 from pix2fen.inference import predict_cells
 from pix2fen.fen import pieces_to_fen
+from pix2fen.crop import crop_chessboard  
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: pix2fen <image>")
+        print("Usage: pix2fen <image>", file=sys.stderr)
         sys.exit(1)
 
-    img = cv2.imread(sys.argv[1])
+    image_path = sys.argv[1]
+    img = cv2.imread(image_path)
     if img is None:
         raise RuntimeError("Image load failed")
 
-    h, w, _ = img.shape
+    board = crop_chessboard(img)
+    h, w, _ = board.shape
     cell_h = h // 8
     cell_w = w // 8
 
     cells = [
-        img[r*cell_h:(r+1)*cell_h, c*cell_w:(c+1)*cell_w]
+        board[r*cell_h:(r+1)*cell_h, c*cell_w:(c+1)*cell_w]
         for r in range(8)
         for c in range(8)
     ]
@@ -25,3 +28,6 @@ def main():
     pieces = predict_cells(cells)
     fen = pieces_to_fen(pieces)
     print(fen)
+
+if __name__ == "__main__":
+    main()
